cc23fd9ac49132eea7e3436cf8c65d406fc43f83
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-08-10 11:20:10 -0500

    add /questions endpoint

--- meteor.py
+++ meteor.py
@@ -15,6 +15,17 @@ app.config.from_object(configs[env])
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello, NASA and the World Bank.'
+
+@app.route('/questions/<int:num>')
+def question(num):
+    questions = {
+        1: 'Q: In 2008, what are the top 5 countries with documented meteor strikes and published scientific technical journals?',
+        2: 'Q: In 2010, what are the top 5 countries with documented meteor strikes and published scientific technical journals?',
+        3: 'Q: What countries are unique to both years?',
+        4: 'Q: What countries are the same from both years?',
+        5: 'Q: Can you infer anything from this data? Why or why not?',
+    }
+    return questions[num]
 #----------------------------------->>>
 
 
