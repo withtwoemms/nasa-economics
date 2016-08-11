01b6f3c726e8834f2472523cb89272f3ca7d44e5
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-08-11 13:52:08 -0500

    more spike progress

--- meteor.py
+++ meteor.py
@@ -3,8 +3,7 @@ import requests
 
 from flask import Flask
 from settings import configs
-from sodapy import Socrates
-from geopy.geocoders import Nominatim
+from sodapy import Socrata
 
 
 app = Flask(__name__)
@@ -50,26 +49,36 @@ def get_country_data_for(formatted_coordinate_pairs):
     return [r.get('results') for r in responses if r.get('status') == 'OK']
 def get_country_names_from(country_data):
     return [(first(item.get('address_components', {})).get('long_name') for item in r if 'country' in item.get('types')).next() for r in country_data]
+
 def get_countries_with_meteorite_landings_in(year):
     coords = format_coordinate_pairs(get_meteorite_landing_coordinates_in(year))
     location_data = get_country_data_for(coords)
     return get_country_names_from(location_data)
-#clean result first
-geolocator = Nominatim()
-countries = countries = [geolocator.reverse("{}, {}".format(item['reclat'], item['reclong'])).address.split(', ') for item in result]
 
 def get_country_id(country_name):
     resp = requests.get('http://api.worldbank.org/countries/all/?per_page=1000&format=json')
     country_ids = [[item.get('name', None), item.get('id', None)] for item in resp.json()[-1]]
     return next((item[-1].encode('utf-8') for item in country_ids if item[0] == country_name), None)
-
-def get_article_data(country_name, year):
+def get_journal_article_indicator_data_for(country_name, year):
     cid = get_country_id(country_name)
     base_url = 'http://api.worldbank.org/countries/{}'.format(cid)
     query = '?per_page=1000&format=json&date={}'.format(year)
     path = '/indicators/IP.JRN.ARTC.SC{}'.format(query)
-    return requests.get(base_url + path).json()[-1][0]
+    return requests.get(base_url + path).json()
+def get_journal_article_indicator_data_for_multiple(country_names, year):
+    result = []
+    for country in country_names:
+        resp = get_journal_article_indicator_data_for(country, year)
+        if not first(resp).get('message'):
+            num_articles = resp[-1][0].get('value')
+            num_articles = num_articles if num_articles else u'0'
+            result.append({
+                'country': country,
+                'num_articles': float(num_articles)
+            })
+    return result
 #----------------------------------->>>
+
 
 if __name__ == '__main__':
     app.run()
